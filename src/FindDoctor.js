7fe2b209e3a6c2ad561b392876abbbd723d53fa6
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-20 06:10:11 -0700

    return doctor locations as well as names.

--- src/FindDoctor.js
+++ src/FindDoctor.js
@@ -2,8 +2,7 @@ export default class FindDoctor {
   findDoctorByConcern(concern) {
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
-      // console.log("Hi");
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=99f6688bd74e87a07cc06f1ef9506ed4`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -4,14 +4,29 @@ import 'bootstrap';
 import FindDoctor from '../src/FindDoctor.js';
 
 $(document).ready(function() {
+
+  function location(practices) {
+    let view = "practice:  ";
+    practices.forEach(function(practice) {
+
+      view += `${practice.name}, Address: ${practice.visit_address.street},  ${practice.visit_address.city}, ${practice.visit_address.state}  `
+
+    });
+    return view;
+  }
   function createDoctorView(data) {
     let view = "Please see the following list of doctors who match your criteria:  ";
 
     data.forEach(function(doctor) {
-      view += doctor.profile.first_name + ' ' + doctor.profile.last_name + ', '
+      view += doctor.profile.first_name + ' ' + doctor.profile.last_name + ', ';
+
+      const locationView = location(doctor.practices);
+
+      view += locationView;
     });
     return view;
   }
+
   $('#getList').click(function() {
     const concern = $('#concern').val();
     $('#concern').val("");
@@ -21,7 +36,7 @@ $(document).ready(function() {
     promise.then(function(response) {
     const body = JSON.parse(response);
     return body;
-
+    // console.log(body);
   }).then(function(body) {
     const doctorView = createDoctorView(body.data);
       $('.showResults').text(doctorView);
